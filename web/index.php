603c66ac9407e3cf07bdc98828026d0b3a3d0d39
Jason Grimes <jason@grimesit.com>
2013-01-19 21:22:19 +0000

    Add DBAL

--- web/index.php
+++ web/index.php
@@ -2,8 +2,30 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-$app = new Silex\Application();
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-// definitions
+$config = require __DIR__ . '/../local.config.php';
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// Register service providers
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_mysql',
+        'dbname' => $config['db_master']['dbname'],
+        'host' => $config['db_master']['host'],
+        'user' => $config['db_master']['user'],
+        'password' => $config['db_master']['pass'],
+    ),
+));
+
+// Routes and controllers
+$app->get('/', function() use ($app) {
+    $data = $app['db']->fetchAll('SHOW DATABASES');
+
+    return new Response(print_r($data, true));
+});
 
 $app->run();
