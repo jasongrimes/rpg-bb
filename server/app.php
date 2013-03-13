cf9364212a147fc95b6e8b08d38ddd2cadeda66c
Jason Grimes <jason@grimesit.com>
2013-03-13 02:44:37 +0000

    Improve error handling of invalid JSON requests. Simplify image base url handling.

--- server/app.php
+++ server/app.php
@@ -10,8 +10,8 @@ use Playground\Playground;
 
 $app = new Application();
 
+// Environment-specific customization
 if (!defined('ENV')) define('ENV', getenv('env') ?: 'prod');
-
 if (ENV == 'dev') {
     $app['debug'] = true;
     ini_set('html_errors', 0);
@@ -19,13 +19,6 @@ if (ENV == 'dev') {
 }
 
 $config = require __DIR__ . '/config/local.config.php';
-
-$app->before(function (Request $request) {
-    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
-        $data = json_decode($request->getContent(), true);
-        $request->request->replace(is_array($data) ? $data : array());
-    }
-});
 
 // Register service providers
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
@@ -39,42 +32,53 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
 ));
 $app['playground_mapper'] = new PlaygroundMapper($app['db']);
 
+// Decode JSON request data
+$app->before(function (Request $request) use ($app) {
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        $data = json_decode($request->getContent(), true);
+
+        // If JSON is invalid, json_decode returns null.
+        if ($data === null && $request->getContent() !== null) {
+            return $app->json(array('error' => 'Invalid JSON.'), 400);
+        }
+
+        $request->request->replace(is_array($data) ? $data : array());
+    }
+});
+
 // -----------------------------------
 // Routes and controllers
 // -----------------------------------
 
-// Route: GET: /
-$app->get('/', function(Application $app) {
+// Route: GET: /hello
+$app->get('/hello', function() {
     return new Response('Hello.');
 });
 
 // Route: GET /playgrounds
 $app->get('/playgrounds', function(Application $app, Request $request) use ($config) {
     $playgrounds = $app['playground_mapper']->getPlaygrounds($request->query->all());
-    return $app->json($playgrounds->toArray($config['image_base_url']));
+    return $app->json($playgrounds->toArray());
 });
 
 // Route: GET /playgrounds/:id
 $app->get('/playgrounds/{id}', function(Application $app, $id) use ($config) {
     $playground = $app['playground_mapper']->getPlayground($id);
-    return $app->json($playground->toArray($config['image_base_url']));
+    return $app->json($playground->toArray());
 });
 
 // Route: POST /playgrounds
 $app->post('/playgrounds', function(Application $app, Request $request) use ($config) {
     $data = json_decode($request->getContent(), true);
-    if (!$data) {
-        return $app->json(array('error' => 'Invalid JSON.'), 400);
-    }
-
     $playground = Playground::createFromArray($data);
+
     if ($app['playground_mapper']->insert($playground)) {
-        return $app->json($playground->toArray($config['image_base_url']), 201); // Should redirect to /playground/id instead?
+        return $app->json($playground->toArray(), 201); // Should redirect to /playground/id instead?
     } else {
         return $app->json(array('error' => $app['playground_mapper']->getLastError()), 400);
     }
 });
-/*
+/* POST with CURL like this:
 curl -H 'Content-Type: application/json' \
     -d '{
         "name": "Test Playground",
@@ -96,7 +100,7 @@ $app->put('/playgrounds/{id}', function(Application $app, Request $request, $id)
     $playground->id = $id;
 
     if ($app['playground_mapper']->update($playground)) {
-        return $app->json($playground->toArray($config['image_base_url']), 200); // Should redirect to /playground/id instead?
+        return $app->json($playground->toArray(), 200); // Should redirect to /playground/id instead?
     } else {
         return $app->json(array('error' => $app['playground_mapper']->getLastError()), 400);
     }
